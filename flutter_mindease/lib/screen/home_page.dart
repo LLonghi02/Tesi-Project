9328467da37e5ca5c26a4ef74c3b98a310a937b0
LLonghi02 <lara.longhi3@gmail.com>
2024-07-03 12:46:53 +0200

    home

--- flutter_mindease/lib/screen/home_page.dart
+++ flutter_mindease/lib/screen/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mindease/widget/bottom_bar.dart';
 import 'package:flutter_mindease/widget/clik_image.dart';
+import 'package:flutter_mindease/widget/font.dart';
 import 'package:flutter_mindease/widget/theme.dart';
 import 'package:flutter_mindease/widget/top_bar.dart';
 
@@ -31,35 +32,61 @@ class HomePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: const TopBar(),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        color: backcolor, // Colore di sfondo preso dal provider
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Ciao, nome',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 170, // Altezza desiderata per l'immagine
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/home.jpg'), 
+                  fit: BoxFit.cover,
+                  alignment: Alignment(0.0, -0.5), // Allinea l'immagine verticalmente in alto
                 ),
-                Icon(Icons.person, size: 32),
-              ],
+              ),
             ),
-            SizedBox(height: 20),
-            Text(
-              'Come ti senti oggi?',
-              style: TextStyle(fontSize: 18),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              color: backcolor, // Colore di sfondo preso dal provider
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Ciao, nome',
+                        style: AppFonts.appTitle,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Container(
+                        color: Colors.white,
+                        padding: const EdgeInsets.all(8.0), // Aggiunto padding per migliorare la visualizzazione
+                        child: Text(
+                          'Come ti senti oggi?',
+                          style: AppFonts.mind,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // Widget per l'obiettivo quotidiano utilizzando ClickableImage
+                  ClickableImage(
+                    imageUrl: 'assets/images/mindfulness.jpg',
+                    text: 'Il tuo obiettivo quotidiano di Mindfulness',
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 20),
-            // Widget per l'obiettivo quotidiano utilizzando ClickableImage
-            ClickableImage(
-              imageUrl: 'assets/images/mindfulness.jpg',
-              text: 'Il tuo obiettivo quotidiano di Mindfulness',
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       bottomNavigationBar: const BottomBar(),
     );
