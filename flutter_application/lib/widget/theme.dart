cf9edec86b4285c6604da7c3c5088a2ca0e7e85b
Lara Longhi <111980294+LLonghi02@users.noreply.github.com>
2024-07-03 15:22:46 +0200

    Delete flutter_application directory

--- flutter_application/lib/widget/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final brightnessProvider = StateProvider<Brightness>((ref) => Brightness.light);
-
-final  accentColorProvider = StateProvider<Color>((ref) =>const Color(0xffd2f7ef));
-final barColorProvider = StateProvider<Color>((ref) =>const Color(0xFFA9EED8));
-
-final detProvider = StateProvider<Color>((ref) =>const Color(0xFF4FA69E));
