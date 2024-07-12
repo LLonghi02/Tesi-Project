0c4176aa1236301913d077acb3c9e5d38db57444
LLonghi02 <lara.longhi3@gmail.com>
2024-07-12 19:41:21 +0200

    suoni ease

--- flutter_app/flutter_mindease/lib/model/sound_model.dart
+++ flutter_app/flutter_mindease/lib/model/sound_model.dart
@@ -1,32 +1,7 @@
 class SuoniModel {
-  final String id;
-  final String title;
-  final String artist;
-  final String album;
-  final String durationSeconds;
-  final String videoUrl;
-  final String tag;
+  final String audioUrl;
 
   SuoniModel({
-    required this.id,
-    required this.title,
-    required this.artist,
-    required this.album,
-    required this.durationSeconds,
-    required this.videoUrl, 
-    required this.tag,
+    required this.audioUrl,
   });
-
-  factory SuoniModel.fromJson(Map<String, dynamic> json) {
-    return SuoniModel(
-      id: json['_id'],
-      title: json['Title'],
-      artist: json['Artist(s)'],
-      album: json['Album'],
-      durationSeconds: json['Duration (ms)'],
-      videoUrl: json['Track URL'],
-      tag: json['Tag'],
-
-    );
-  }
 }
--- flutter_app/flutter_mindease/lib/screen/ease/suoni.dart
+++ flutter_app/flutter_mindease/lib/screen/ease/suoni.dart
@@ -1,49 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_mindease/model/sound_model.dart';
-import 'package:flutter_mindease/provider/theme.dart';
-import 'package:flutter_mindease/repository/soundProvider.dart';
 import 'package:flutter_mindease/widget/bottom_bar.dart';
 import 'package:flutter_mindease/widget/top_bar.dart';
-import 'package:flutter_mindease/screen/soundPlayer.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-class SoundPage extends ConsumerWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class SoundPage extends StatefulWidget {
   const SoundPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final backcolor = ref.watch(accentColorProvider);
-    final AsyncValue<List<SuoniModel>> soundListAsyncValue = ref.watch(soundProvider('suoni'));
+  _SoundPageState createState() => _SoundPageState();
+}
 
+class _SoundPageState extends State<SoundPage> {
+  late AudioPlayer _player;
+  final List<String> _audioFiles = [
+    'assets/sound/Beach Waves.mp3',
+    'assets/sound/Beach.mp3',
+    'assets/sound/Bosco & Uccelli.mp3',
+    'assets/sound/Cinguettio rilassante.mp3',
+    'assets/sound/Fire Crackle At the Lake.mp3',
+    'assets/sound/Gentle Rivers and Streams.mp3',
+    'assets/sound/Hazard\'s Beach.mp3',
+    'assets/sound/Mountain Rain.mp3',
+    'assets/sound/Musica di rilassamento con uccelli.mp3',
+    'assets/sound/Natural Meditation.mp3',
+    'assets/sound/Nature Sounds of the Ocean.mp3',
+    'assets/sound/Ocean Relax.mp3',
+    'assets/sound/Ocean Waves .mp3',
+    'assets/sound/Oltre le nuvole (suono rilassante di uccelli).mp3',
+    'assets/sound/Piccoli uccellini.mp3',
+    'assets/sound/Piccoli volatili.mp3',
+    'assets/sound/Relaxing Rain.mp3',
+    'assets/sound/River, Thunder & Rain - Calm Excitement.mp3',
+    'assets/sound/Soft Sounds of the Night.mp3',
+    'assets/sound/Suburban Forest Rain 2.mp3',
+    'assets/sound/Suoni degli uccelli.mp3',
+    'assets/sound/The Babbling Brook - Scotland.mp3',
+    'assets/sound/The Sound Of The Jungle With Coloured Birds For Relaxation, Sleeping, Studying.mp3',
+    'assets/sound/Thunderstorm and Rain Music.mp3',
+    'assets/sound/Tranquil Babbling Brook for Deep Sleep.mp3',
+    'assets/sound/White Noise Rain and River.mp3',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _player = AudioPlayer();
+    _initPlayers();
+  }
+
+  Future<void> _initPlayers() async {
+    try {
+      // Carica e prepara tutti i file audio
+      await Future.wait(_audioFiles.map((url) => _player.setAsset(url)));
+    } catch (e) {
+      print('Error initializing players: $e');
+      // Gestisci eventuali errori di inizializzazione qui
+    }
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  void _playAudio(String audioUrl) {
+    _player.setAsset(audioUrl).then((_) {
+      _player.play();
+    }).catchError((error) {
+      print('Error playing audio: $error');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backcolor,
-      appBar: const TopBar(),
-      body: soundListAsyncValue.when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stackTrace) => Center(child: Text('Error: $error')),
-        data: (soundList) {
-          if (soundList.isEmpty) {
-            return const Center(child: Text('No data available'));
-          } else {
-            return ListView.builder(
-              itemCount: soundList.length,
-              itemBuilder: (context, index) {
-                final video = soundList[index];
-                return ListTile(
-                  title: Text(video.title),
-                  subtitle: Text(video.artist),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => SuoniPlayerPage(videoUrl: video.videoUrl),
-                    ));
-                  },
-                );
-              },
-            );
-          }
+      appBar: TopBar(),
+      body: ListView.builder(
+        itemCount: _audioFiles.length,
+        itemBuilder: (context, index) {
+          final audioFile = _audioFiles[index];
+          return ListTile(
+            title: Text(audioFile.split('/').last), // Mostra solo il nome del file
+            onTap: () {
+              _playAudio(audioFile);
+            },
+          );
         },
       ),
-      bottomNavigationBar: const BottomBar(),
+            bottomNavigationBar: const BottomBar(),
+
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Audio Player Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SoundPage(),
     );
   }
 }
