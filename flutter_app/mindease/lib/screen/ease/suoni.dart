d63c2f44bb6fa16b875b863b707daec51ea9db05
LLonghi02 <lara.longhi3@gmail.com>
2024-07-15 10:34:57 +0200

    aggiunta suoni

--- flutter_app/mindease/lib/screen/ease/suoni.dart
+++ flutter_app/mindease/lib/screen/ease/suoni.dart
@@ -11,20 +11,35 @@ final audioPlayerProvider = Provider<AudioPlayer>((ref) => AudioPlayer());
 class SoundPage extends ConsumerWidget {
   SoundPage({Key? key}) : super(key: key);
 
-  final List<Map<String, dynamic>> audioFiles = [
-    {'index': 0, 'filePath': 'assets/sound/Beach Waves.mp3'},
-    {'index': 1, 'filePath': 'assets/sound/Beach.mp3'},
-    {'index': 2, 'filePath': 'assets/sound/Bosco & Uccelli.mp3'},
-    {'index': 3, 'filePath': 'assets/sound/Cinguettio rilassante.mp3'},
-    {'index': 4, 'filePath': 'assets/sound/Fire Crackle At the Lake.mp3'},
-    {'index': 5, 'filePath': 'assets/sound/Gentle Rivers and Streams.mp3'},
-    {'index': 6, 'filePath': 'assets/sound/Hazard Beach.mp3'},
-    {'index': 7, 'filePath': 'assets/sound/Mountain Rain.mp3'},
-    {'index': 8, 'filePath': 'assets/sound/Musica di rilassamento con uccelli.mp3'},
-    {'index': 9, 'filePath': 'assets/sound/Natural Meditation.mp3'},
-    {'index': 10, 'filePath': 'assets/sound/Nature Sounds of the Ocean.mp3'},
-    {'index': 11, 'filePath': 'assets/sound/Ocean Relax.mp3'},
-  ];
+final List<Map<String, dynamic>> audioFiles = [
+  {'index': 0, 'filePath': 'assets/sound/Beach Waves.mp3'},
+  {'index': 1, 'filePath': 'assets/sound/Beach.mp3'},
+  {'index': 2, 'filePath': 'assets/sound/Bosco & Uccelli.mp3'},
+  {'index': 3, 'filePath': 'assets/sound/Cinguettio rilassante.mp3'},
+  {'index': 4, 'filePath': 'assets/sound/Fire Crackle At the Lake.mp3'},
+  {'index': 5, 'filePath': 'assets/sound/Gentle Rivers and Streams.mp3'},
+  {'index': 6, 'filePath': 'assets/sound/Hazard Beach.mp3'},
+  {'index': 7, 'filePath': 'assets/sound/Mountain Rain.mp3'},
+  {'index': 8, 'filePath': 'assets/sound/Musica di rilassamento con uccelli.mp3'},
+  {'index': 9, 'filePath': 'assets/sound/Natural Meditation.mp3'},
+  {'index': 10, 'filePath': 'assets/sound/Nature Sounds of the Ocean.mp3'},
+  {'index': 11, 'filePath': 'assets/sound/Ocean Relax.mp3'},
+  {'index': 12, 'filePath': 'assets/sound/Ocean Waves .mp3'},
+  {'index': 13, 'filePath': 'assets/sound/Oltre le nuvole (suono rilassante di uccelli).mp3'},
+  {'index': 14, 'filePath': 'assets/sound/Piccoli uccellini.mp3'},
+  {'index': 15, 'filePath': 'assets/sound/Piccoli volatili.mp3'},
+  {'index': 16, 'filePath': 'assets/sound/Relaxing Rain.mp3'},
+  {'index': 17, 'filePath': 'assets/sound/River, Thunder & Rain - Calm Excitement.mp3'},
+  {'index': 18, 'filePath': 'assets/sound/Soft Sounds of the Night.mp3'},
+  {'index': 19, 'filePath': 'assets/sound/Suburban Forest Rain 2.mp3'},
+  {'index': 20, 'filePath': 'assets/sound/Suoni degli uccelli.mp3'},
+  {'index': 21, 'filePath': 'assets/sound/The Babbling Brook - Scotland.mp3'},
+  {'index': 22, 'filePath': 'assets/sound/The Sound Of The Jungle With Coloured Birds For Relaxation, Sleeping, Studying.mp3'},
+  {'index': 23, 'filePath': 'assets/sound/Thunderstorm and Rain Music.mp3'},
+  {'index': 24, 'filePath': 'assets/sound/Tranquil Babbling Brook for Deep Sleep.mp3'},
+  {'index': 25, 'filePath': 'assets/sound/White Noise Rain and River.mp3'},
+];
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
