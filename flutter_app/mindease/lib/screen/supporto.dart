92b4bdf46aa0738c2d544089855ba12dcf017bd7
LLonghi02 <lara.longhi3@gmail.com>
2024-07-15 13:19:19 +0200

    messaggi predefiniti per la chat

--- flutter_app/mindease/lib/screen/supporto.dart
+++ flutter_app/mindease/lib/screen/supporto.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:mindease/provider/main_support.dart';
-import 'package:mindease/provider/theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mindease/provider/main_support.dart';
+import 'package:mindease/provider/message.dart';
+import 'package:mindease/provider/theme.dart';
+
 import 'package:mindease/widget/bottom_bar.dart';
 import 'package:mindease/widget/top_bar.dart';
 
@@ -14,37 +18,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MindEase',
-      home: SupportoPage(),
+    return const ProviderScope(
+      child: MaterialApp(
+        title: 'MindEase',
+        home: SupportoPage(),
+      ),
     );
   }
 }
 
 const String defaultBackgroundImage = 'assets/images/supporto.png';
 
-class Message {
-  final String text;
-
-  Message(this.text);
-}
-
 class SupportoPage extends ConsumerWidget {
-  final List<Message> messages = [];
-
-  SupportoPage({Key? key}) : super(key: key);
-
-  void userMessage(String text) {
-    if (text.isNotEmpty) {
-      messages.add(Message(text));
-    }
-  }
+  const SupportoPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final backcolor = ref.watch(accentColorProvider);
-    final backgroundImage =
-        ref.watch(backgroundImageProvider) ?? defaultBackgroundImage;
+    final backgroundImage = ref.watch(backgroundImageProvider) ?? defaultBackgroundImage;
+
+    final List<Message> messages = ref.watch(messageListProvider);
+
+
+    void userMessage(String text) {
+      if (text.isNotEmpty) {
+        ref.read(messageListProvider.notifier).addMessage(Message(text));
+      }
+    }
+
+    TextEditingController textController = TextEditingController();
+
 
     return Scaffold(
       appBar: const TopBar(),
@@ -71,27 +74,31 @@ class SupportoPage extends ConsumerWidget {
             bottom: 0,
             child: Container(
               padding: const EdgeInsets.all(8.0),
-              
               child: Row(
                 children: [
                   Expanded(
                     child: TextField(
+                      controller: textController,
                       decoration: const InputDecoration(
                         hintText: 'Cosa ti causa ansia?',
                         border: OutlineInputBorder(),
                         fillColor: Colors.white,
                         filled: true,
                       ),
-                      onSubmitted: userMessage,
+                      textInputAction: TextInputAction.send,
+                      // Use TextInputAction.send to enable Enter key to send the message
                     ),
                   ),
                   IconButton(
                     icon: const Icon(Icons.send),
                     onPressed: () {
-                      if (messages.isNotEmpty) {
-                        userMessage(messages.last.text);
+                      String messageText = textController.text.trim(); // Leggere il testo attuale nel TextField
+                      if (messageText.isNotEmpty) {
+                        print("Sending message from IconButton: $messageText"); // Controllo per il messaggio dall'IconButton
+                        userMessage(messageText);
+                        textController.clear(); // Pulire il TextField dopo l'invio del messaggio
                       } else {
-                        // Gestione nel caso in cui non ci siano messaggi
+                        print("Messaggio vuoto"); 
                       }
                     },
                   ),
@@ -106,7 +113,7 @@ class SupportoPage extends ConsumerWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: messages
-                  .map((message) => _buildMessageBubble(message.text))
+                  .map((message) => _buildMessageBubble(context,message))
                   .toList(),
             ),
           ),
@@ -116,15 +123,44 @@ class SupportoPage extends ConsumerWidget {
     );
   }
 
-  Widget _buildMessageBubble(String text) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
+
+  Widget _buildMessageBubble(BuildContext context, Message message) {
+    final isUserMessage = message.isUser;
+  final alignment = isUserMessage ? Alignment.centerRight : Alignment.centerLeft;
+    final backgroundColor = isUserMessage ? Colors.white : Colors.blueGrey;
+    final textColor = isUserMessage ? Colors.black : Colors.white;
+    final borderRadius = isUserMessage
+        ? const BorderRadius.only(
+            topLeft: Radius.circular(12.0),
+            topRight: Radius.circular(12.0),
+            bottomRight: Radius.circular(12.0),
+          )
+        : const BorderRadius.only(
+            topLeft: Radius.circular(12.0),
+            topRight: Radius.circular(12.0),
+            bottomLeft: Radius.circular(12.0),
+          );
+
+    return Align(
+      alignment:alignment ,
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 300),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: borderRadius,
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        child: Text(
+          message.text,
+          style: TextStyle(
+            color: textColor,
+          ),
+        ),
       ),
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      child: Text(text),
     );
   }
+
+
+
 }
